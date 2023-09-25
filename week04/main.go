a6b5955a12c206e4381233bcdaff07ad36e36a49
202344053 <kimyh040624@naver.com>
2023-09-25 16:45:42 +0900

    v0

---
+++ week04/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"reflect"
+	"strings"
+)
+
+func main() {
+	fmt.Println("_________________________________________________________")
+	fmt.Println()
+
+	a := 7
+	fmt.Println(a, reflect.TypeOf(a))
+
+
+	// b := 8.34	// float64
+	var  b float32 = 8.34
+	fmt.Println(b, reflect.TypeOf(b))
+
+
+	fmt.Println(a * int(b))
+	fmt.Println(float32(a) > b)
+	
+
+	var c7 string	// 변수명은 알파벳 대소문자로 시작해야 한다.
+	var d int
+	var e bool
+	var f float64
+	var G = 99
+
+	// koreanzombie := "정찬성" // 문법적으론 문제 없지만 camel 케이스 관례를 따르자.
+	koreanZombie := "정찬성" 	
+	fmt.Println(koreanZombie)
+
+	fmt.Println(c7, d, e, f, G)
+
+	// var a int	// declaration
+	// a = 7		// assign value
+
+	// var a int = 7	// declaration & assign value
+	// var a = 7	// declaration & assign value
+
+
+	fmt.Println('Z', '2', '\n', '김', '인', '하')	// rune literals(int32) 90, 50 , 10 , 44608, 51064, 54616
+	fmt.Println(reflect.TypeOf('Z'), reflect.TypeOf(2), reflect.TypeOf("Hi"), reflect.TypeOf(4.99), reflect.TypeOf(false))
+	// fmt.Println(math.Floor(삼.오), math.Ceil(2.17), math.Sqrt(십육))
+	// fmt.Println(strings.Title(3.141592))
+	fmt.Println(math.Floor(2.17), math.Ceil(2.17), math.Sqrt(16))
+	fmt.Println(strings.Title("open source\tprogramming\n\"go\"!"))
+	// fmt.Println(strings.Title("오픈소스 프로그래밍"))
+	// fmt.Println("OpenSource Programming~", "Go")
+}
